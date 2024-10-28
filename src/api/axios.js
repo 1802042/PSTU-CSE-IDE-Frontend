79515e4422b8ff522386655ed8bec42a782b0142
Knightshade <rony16@cse.pstu.ac.bd>
2024-10-29 05:48:59 +0600

    Update API base URL to use remote server

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,5 +1,5 @@
 import axios, { isCancel } from "axios";
-const BASE_URL = "http://localhost:8000/api/v1";
+const BASE_URL = "http://52.140.17.58:8000/api/v1";
 
 export default axios.create({
   baseURL: BASE_URL,
